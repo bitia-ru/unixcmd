dfdf98c82fc809a52cc71b03976bc66ab27fe256
Artem Levenkov <alevenkov@networkoptix.com>
2024-07-26 04:13:40 +0200

    Reset the project again in favor of native realizations. Add GTK4 project.

--- conan/profiles/ios.profile
+++
@@ -1,8 +0,0 @@
-[settings]
-os=iOS
-os.version=12.0
-arch=armv8
-compiler=apple-clang
-compiler.version=11.0
-compiler.libcxx=libc++
-build_type=Release
---
+++ gtk/main.c
@@ -0,0 +1,21 @@
+#include <gtk/gtk.h>
+
+static void on_activate(GApplication* app, gpointer user_data) {
+  GtkWidget* window = gtk_application_window_new(GTK_APPLICATION(app));
+  gtk_window_set_title(GTK_WINDOW(window), "UnixCMD");
+  gtk_window_set_default_size(GTK_WINDOW(window), 600, 500);
+
+  gtk_widget_show(window);
+}
+
+int main(int argc, char **argv) {
+  GtkApplication *app = gtk_application_new("app.unixcmd", G_APPLICATION_FLAGS_NONE);
+
+  g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
+
+  int status = g_application_run(G_APPLICATION(app), argc, argv);
+
+  g_object_unref(app);
+
+  return status;
+}
