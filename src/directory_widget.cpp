d51b063da9bcb47002c5bd77933eef8ff50b0758
ALev <me@alev.pro>
2025-02-01 14:34:42 +0300

    Shorten Directory Panel title path if needed

--- src/directory_widget.cpp
+++ src/directory_widget.cpp
@@ -6,20 +6,143 @@
 #include <QLabel>
 #include <QVBoxLayout>
 
+struct PathComponent
+{
+    QString text;
+    bool isHome = false;
+    bool isShortened = false;
+};
+
+Q_DECLARE_METATYPE(PathComponent)
+
 
 struct DirectoryWidget::Private
 {
     QLabel* title = nullptr;
     DirectoryView* view = nullptr;
 
-    void setTitle(const QString& dirPath)
+    void setTitle(const QDir& dir)
     {
-        QString resultPath(dirPath);
+        QVariantList resultComponents;
 
-        if (const auto homePath = QDir::homePath(); resultPath.startsWith(homePath))
-            resultPath.replace(0, homePath.size(), "🏠");
+        for (const auto& entry : dir.absolutePath().split(dir.separator()))
+            resultComponents << entry;
 
-        title->setText(resultPath);
+        const auto home = QDir::home();
+        const auto homeComponents = home.absolutePath().split(home.separator());
+
+        if (homeComponents.count() < resultComponents.count()) {
+            QStringList componentStrings;
+            for (const auto& component : resultComponents.first(homeComponents.count()))
+                componentStrings << component.toString();
+
+            if (componentStrings == home.absolutePath().split(home.separator())) {
+                resultComponents.remove(0, homeComponents.count());
+                resultComponents.prepend(QVariant::fromValue(PathComponent{
+                    .text = "~",
+                    .isHome = true,
+                }));
+            }
+        }
+
+        const QFontMetrics metrics(title->font());
+
+        const auto resultPathPlainText = [&resultComponents] -> QString {
+            QStringList resultPathComponents;
+
+            for (const auto& component : resultComponents) {
+                if (component.typeId() == QMetaType::QString) {
+                    resultPathComponents << component.toString();
+                } else if (component.userType() == qMetaTypeId<PathComponent>()) {
+                    resultPathComponents << component.value<PathComponent>().text;
+                } else {
+                    Q_ASSERT(false);
+                }
+            }
+
+            return resultPathComponents.join("/");
+        };
+
+        const auto textRect = [this, &resultPathPlainText, &metrics] -> QRect {
+            return metrics.boundingRect(
+                QRect(0, 0, title->width(), title->height()),
+                Qt::TextSingleLine,
+                resultPathPlainText()
+            );
+        };
+
+         while (textRect().width() > title->width() - 10) {
+            const auto findComponentToShorten = [&resultComponents] -> int {
+                int i = 0;
+                int middleIndex = resultComponents.count() / 2;
+                const auto component = resultComponents.at(middleIndex);
+
+                const auto componentAlreadyShortened = [](const QVariant& component) -> bool {
+                    return component.userType() == qMetaTypeId<PathComponent>()
+                        && component.value<PathComponent>().isShortened;
+                };
+
+                do {
+                    if (
+                        componentAlreadyShortened(resultComponents.at(middleIndex + i)) &&
+                        componentAlreadyShortened(resultComponents.at(middleIndex - i))
+                    ) {
+                        i++;
+                        continue;
+                    }
+
+                    if (componentAlreadyShortened(resultComponents.at(middleIndex + i)))
+                        return middleIndex - i;
+
+                    return middleIndex + i;
+                } while (middleIndex + i < resultComponents.count() && middleIndex - i >= 0);
+
+                return -1;
+            };
+
+            const int i = findComponentToShorten();
+            if (i < 0 || i >= resultComponents.count()-1)
+                break;
+
+            const auto componentToShorten = resultComponents.at(i);
+
+            if (componentToShorten.typeId() == QMetaType::QString) {
+                resultComponents.replace(i, QVariant::fromValue(PathComponent{
+                    .text = componentToShorten.toString().first(1),
+                    .isShortened = true,
+                }));
+            } else if (componentToShorten.userType() == qMetaTypeId<PathComponent>()) {
+                auto pathComponent = componentToShorten.value<PathComponent>();
+                pathComponent.isShortened = true;
+                resultComponents.replace(i, QVariant::fromValue(pathComponent));
+            } else {
+                Q_ASSERT(false);
+            }
+        }
+
+        QStringList resultPathComponents;
+
+        for (const auto& component : resultComponents) {
+            if (component.typeId() == QMetaType::QString) {
+                resultPathComponents << component.toString();
+            } else if (component.userType() == qMetaTypeId<PathComponent>()) {
+                const auto pathComponent = component.value<PathComponent>();
+
+                if (pathComponent.isHome) {
+                    resultPathComponents << QString("<span style=\"color:blue;font-weight:900;\">%1</span>")
+                        .arg(pathComponent.text);
+                } else if (pathComponent.isShortened) {
+                    resultPathComponents << QString("<span style=\"color:grey;font-weight:100;\">%1</span>")
+                        .arg(pathComponent.text);
+                }
+            } else {
+                Q_ASSERT(false);
+            }
+        }
+
+        title->setText(
+            resultPathComponents.join("<font color='blue'>/</font>")
+        );
     }
 };
 
@@ -30,6 +153,8 @@ DirectoryWidget::DirectoryWidget(QWidget* parent)
         .view = new DirectoryView(this),
     })
 {
+    d->title->setFont(QFont("MesloLGS NF", 12));
+    d->title->setTextFormat(Qt::MarkdownText);
     d->title->setContentsMargins(4, 0, 0, 0);
 
     auto layout = new QVBoxLayout(this);
@@ -39,9 +164,9 @@ DirectoryWidget::DirectoryWidget(QWidget* parent)
     setLayout(layout);
 
     connect(d->view, &DirectoryView::directoryChanged, [this](const QDir& directory) {
-        d->setTitle(directory.absolutePath());
+        d->setTitle(directory);
     });
-    d->setTitle(d->view->directory().absolutePath());
+    d->setTitle(d->view->directory());
 }
 
 DirectoryWidget::~DirectoryWidget() = default;
@@ -49,4 +174,11 @@ DirectoryWidget::~DirectoryWidget() = default;
 DirectoryView* DirectoryWidget::view() const
 {
     return d->view;
+}
+
+void DirectoryWidget::resizeEvent(QResizeEvent* event)
+{
+    QWidget::resizeEvent(event);
+
+    d->setTitle(d->view->directory());
 }
--- src/directory_widget.h
+++ src/directory_widget.h
@@ -17,6 +17,9 @@ public:
     DirectoryView* view() const;
 
 private:
+    void resizeEvent(QResizeEvent* event) override;
+
+private:
     struct Private;
     QScopedPointer<Private> d;
 };
