7e23017a4baf94d48102ec78fa900c7297c77e70
ALev <me@alev.pro>
2025-05-27 06:56:35 +0200

    Move macOS distribution signing to github ci

--- src/cmake/distribution.cmake
+++ src/cmake/distribution.cmake
@@ -2,7 +2,7 @@ function(install_license)
   if(APPLE)
     install(
       FILES ${CMAKE_SOURCE_DIR}/LICENSE
-      DESTINATION "${CMAKE_INSTALL_BINDIR}/${PROJECT_NAME}.app/Contents/Resources"
+      DESTINATION "${CMAKE_INSTALL_BINDIR}/../${PROJECT_NAME}.app/Contents/Resources"
     )
   else()
     install(
